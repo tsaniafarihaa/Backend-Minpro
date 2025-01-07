a30087f9d4370002e2ce588e74d3c53397bafb34
crayoninvok <dathariqf@gmail.com>
2025-01-07 15:40:35 +0700

    change the point to the verify

---
+++ dist/utils/getWeekNumber.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getWeekNumber = getWeekNumber;
+function getWeekNumber(date) {
+    const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+    const pastDaysOfYear = (date.getTime() - firstDayOfYear.getTime()) / (24 * 60 * 60 * 1000);
+    return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+}
